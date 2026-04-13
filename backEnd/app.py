87764735295020bfed192bb43c93e658574955d4
Devanandanan C <nandannand2003@gmail.com>
2026-04-13 13:53:01 +0530

    fully functioning task app

---
+++ backEnd/app.py
@@ -0,0 +1,32 @@
+from flask import Flask,request,jsonify
+from pymongo import MongoClient
+from flask_cors import CORS
+
+app=Flask(__name__)
+CORS(app)
+
+client=MongoClient("mongodb+srv://nandannand2003_db_user:fO7X1VRhgUgEgtha@cluster0.p0h3vcw.mongodb.net/?appName=Cluster0")
+db=client["Student"]
+collection=db["collection"]
+
+@app.route("/task",methods=["GET"])
+def get_task():
+    data=list(collection.find({},{"_id":0}))
+    return jsonify(data)
+
+@app.route("/task",methods=["POST"])
+def add_task():
+    data=request.json
+    collection.insert_one(data)
+    return jsonify({"msg":"task added"})
+
+@app.route("/task/<title>",methods=["DELETE"])
+def delete_task(title):
+    collection.delete_one({"title":title})
+    return jsonify({"msg":"task deleted"})
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
+
